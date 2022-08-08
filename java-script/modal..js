36ec191a91567364b5197ff7243a669216850395
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-08 13:05:17 +0300

    js updated

--- java-script/modal..js
+++ java-script/modal..js
@@ -5,12 +5,30 @@
         modal: document.querySelector("[data-modal]"),
     };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", openModal);
+    refs.closeModalBtn.addEventListener("click", closeModal);
+    refs.modal.addEventListener('click', closeOnBackdrop)
 
-    function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
+    function openModal() {
+        refs.modal.classList.remove("is-hidden");
+        window.addEventListener('keydown', escapeOut)
     }
 
+    function closeModal() {
+        refs.modal.classList.add("is-hidden");
+        window.removeEventListener('keydown', escapeOut)
+    }
 
+    function closeOnBackdrop(event) {
+        if (event.currentTarget === event.target) {
+            closeModal();
+        }
+    }
+
+    function escapeOut(event) {
+        console.log(event);
+        if (event.code === 'Escape') {
+            closeModal()
+        }
+    }
 })();
