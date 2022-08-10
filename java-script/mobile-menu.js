0590ae123efef8dfea13b38d0aeb4cee497bb1d7
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-10 10:09:37 +0300

    filter.button.adjusted

--- java-script/mobile-menu.js
+++ java-script/mobile-menu.js
@@ -12,3 +12,36 @@
         mobileMenuRef.classList.toggle("is-open");
     });
 })();
+
+
+const filterBtns = document.querySelector('.filter.list');
+
+const productItems = document.querySelectorAll('[data-value]');
+
+console.log(productItems);
+filterBtns.addEventListener('click', onClickFilter)
+
+
+function onClickFilter(event) {
+    if (event.target.nodeName !== 'BUTTON') {
+        return
+    }
+
+    productItems.forEach(v => {
+        v.style.display = '';
+    })
+
+    if (event.target.dataset.filter === 'all') {
+        return
+    }
+
+    const result = [...productItems].filter(v => {
+        return v.dataset.value !== event.target.dataset.filter
+    })
+
+    result.forEach(v => {
+        v.style.display = 'none';
+    })
+
+
+}
